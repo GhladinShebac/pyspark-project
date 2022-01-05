7fa50f5488e0dea8ddec6b7aca1280f4d61cc785
Ghladin Shebac <80664124+GhladinShebac@users.noreply.github.com>
2022-01-05 11:27:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-#import pandas as pd
 import streamlit as st 
-#from pyspark.sql import SparkSession
-#from pyspark.ml.feature import OneHotEncoder, StringIndexer, VectorAssembler
-#from pyspark.ml import Pipeline
-#from pyspark.ml.classification import RandomForestClassifier
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
+
 df = pd.read_csv("bank.csv")
 df = pd.get_dummies(df, columns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome'])
 df['deposit'] = LabelEncoder().fit_transform(df['deposit'])
@@ -16,34 +11,8 @@ X, y  = df.drop('deposit', axis=1), df['deposit']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 clf = LogisticRegression(random_state=0).fit(X_train, y_train) 
 
-#spark = SparkSession.builder.appName('ml-bank').getOrCreate()
-#df = spark.read.csv('bank.csv', header = True, inferSchema = True)
-#cols = df.columns
-#categoricalColumns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'poutcome']
-#stages = []
-#for categoricalCol in categoricalColumns:
-#    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
-#    encoder = OneHotEncoder(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
-#    stages += [stringIndexer, encoder]
-#label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
-#stages += [label_stringIdx]
-#numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
-#assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
-#assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
-#stages += [assembler]
-#pipeline = Pipeline(stages = stages)
-#pipelineModel = pipeline.fit(df)
-#df = pipelineModel.transform(df)
-#selectedCols = ['label', 'features'] + cols
-#df = df.select(selectedCols)
-#train, test = df.randomSplit([0.7, 0.3], seed = 2018)
-#rf = RandomForestClassifier(featuresCol = 'features', labelCol = 'label')
-#rfModel = rf.fit(df)
-#predictions = rfModel.transform(df)
-
 st.write("""
-# Bank
-describ.
+# PySpark Project.
 """)
 
 def user_input_var():
@@ -63,8 +32,7 @@ def user_input_var():
   pdays = st.slider('pdays', -1, 1000)
   previous = st.slider('previous', 0, 60)
   poutcome = st.selectbox('poutcome', ('unknown', 'other', 'failure', 'success'))
-  #deposit = st.radio("deposit", ('no', 'yes'))
-
+  
   data = {'age': age, 'job': job, 'marital': marital, 'education': education,
         'default': default, 'balance': balance, 'housing': housing,
         'loan': loan, 'contact': contact, 'day': day, 'month': month,
@@ -74,30 +42,9 @@ def user_input_var():
   return features
 
 df = user_input_var()
-df = spark.createDataFrame(df)
 df = pd.get_dummies(df, columns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome'])
-#cols = df.columns
-#categoricalColumns = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'poutcome']
-#stages = []
-#for categoricalCol in categoricalColumns:
-#    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
-#    encoder = OneHotEncoder(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
-#    stages += [stringIndexer, encoder]
-#label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
-#stages += [label_stringIdx]
-#numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
-#assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
-#assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
-#stages += [assembler]
-#pipeline = Pipeline(stages = stages)
-#pipelineModel = pipeline.fit(df)
-#df = pipelineModel.transform(df)
-#selectedCols = ['label', 'features'] + cols
-#df = df.select(selectedCols)
-
 
 if st.button('Predict'):
     pred = clf.predict(df)
-    #pred = rfModel.transform(df)
     st.write(pred)
 else: pass
